20e5437448f13cbae4e5f192a6bba49ac8847365
Anton <141570541+Anonymusm@users.noreply.github.com>
2026-07-06 20:03:38 +0300

    started

---
+++ src/components/Report/Report.tsx
@@ -0,0 +1,49 @@
+import s from "./Report.module.css";
+import array from "../../../public/data/report.json";
+import {
+  Products,
+  Alcohol,
+  Fun,
+  Health,
+  Transport,
+  Home,
+  Technic,
+  Bills,
+  Learn,
+  Hobby,
+  Other,
+} from "../icons/Icons";
+
+export function Report() {
+  const icons = array;
+
+  const iconMap = {
+    Products,
+    Alcohol,
+    Fun,
+    Health,
+    Transport,
+    Home,
+    Technic,
+    Bills,
+    Hobby,
+    Learn,
+    Other,
+  };
+  
+  return (
+    <section className={s.section}>
+      <ul className={s.list}>
+        {icons.map((icon) => {
+          const Icon = iconMap[icon.id as keyof typeof iconMap];
+          return (
+            <li key={icon.id}>
+              {Icon && <Icon />}
+              <h3 className={s.iconName}>{icon.title.toUpperCase()}</h3>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
