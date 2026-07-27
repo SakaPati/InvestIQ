fd21e134a5b21491e1abd5297adb82d5f8feb115
Denis Karatyha <karatygad@gmail.com>
2026-07-27 19:49:47 +0200

    fix

--- frontend/src/components/utils/container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import s from "./Container.module.css"
-
-interface ContainerProps {
-    children: React.ReactNode
-}
-
-export const Container = ({ children }: ContainerProps) => {
-    return <div className={s.container}>{children}</div>;
-}
