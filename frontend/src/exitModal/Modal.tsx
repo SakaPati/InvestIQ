3c8c5713c9c7be6d884509af39366ff19c61528f
DesertKnight <146014154+Poseydon12@users.noreply.github.com>
2026-08-07 21:21:22 +0200

    quick fix

--- frontend/src/exitModal/Modal.tsx
+++ frontend/src/exitModal/Modal.tsx
@@ -1,4 +1,4 @@
-import { IoCloseSharp } from "react-icons/io5"; // 1. Import the icon
+import { IoCloseSharp } from "react-icons/io5"; 
 import s from './Modal.module.css';
 
 interface ModalProps {
@@ -14,7 +14,7 @@ export const Modal = ({ isOpen, title, onConfirm, onCancel }: ModalProps) => {
   return (
     <div className={s.backdrop} onClick={onCancel}>
       <div className={s.modal} onClick={(e) => e.stopPropagation()}>
-        <button className={s.closeButton} onClick={onCancel}></button>
+        <button className={s.closeButton} onClick={onCancel}><IoCloseSharp/></button>
         <h2 className={s.title}>{title}</h2>
         <div className={s.actions}>
           <button className={s.confirmBtn} onClick={onConfirm}>ТАК</button>
