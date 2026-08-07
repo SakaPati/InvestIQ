1647d04c4a0c1abfa594dc4f928a381abb175fa3
Fozeton <karatygad@gmail.com>
2026-08-07 14:59:18 +0200

    Added: routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
-import { Login } from "./components/Authorization/login/Login"
-import { Home } from "./components/Home/Home"
-import { Report } from "./components/Report/Report"
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./components/Authorization/login/Login";
+import { Home } from "./components/Home/Home";
+import { Report } from "./components/Report/Report";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
+import { NotFound } from "./components/NotFound/NotFound";
 
 function App() {
-
   return (
-    <>
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
-      <Home />
-      {/* <Login /> */}
-      {/* <Report /> */}
-    </>
-  )
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
-import { store } from './redux/store.ts'
+import { BrowserRouter } from 'react-router-dom'
+import { store, persistor } from './redux/store.ts'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const CLIETN_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/InvestIQ/">
-        <GoogleOAuthProvider clientId={CLIETN_ID}>
-          <App />
-        </GoogleOAuthProvider>
-      </BrowserRouter>
-    </ Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/InvestIQ/">
+          <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <App />
+          </GoogleOAuthProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
