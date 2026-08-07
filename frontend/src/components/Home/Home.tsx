35dba14261affec8745b53b482a7a4f9c2d6a3ae
Fozeton <karatygad@gmail.com>
2026-08-07 13:54:31 +0200

    Added: save balance in home page

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,21 +1,62 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Graf } from "@/assets";
 import { Container } from "../utils/container/Container";
-import s from "./Home.module.css"
+import type { RootState, AppDispatch } from "@/redux/store";
+import { login } from "@/redux/slices/user";
+import s from "./Home.module.css";
 
 export const Home = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const user = useSelector((state: RootState) => state.user);
+    const [balanceInput, setBalanceInput] = useState<string>("");
+
+    useEffect(() => {
+        if (user.balance !== null && user.balance !== undefined) {
+            setBalanceInput(String(user.balance));
+        }
+    }, [user.balance]);
+
+    const handleConfirmBalance = () => {
+        const numericBalance = Number(balanceInput);
+
+        if (isNaN(numericBalance)) return;
+
+        dispatch(
+            login({
+                ...user,
+                balance: numericBalance,
+            })
+        );
+    };
+
     return (
         <section>
             <Container>
                 <div className={s.test}>
-                    <p className={s.calculations}>Перейти до розрахунків <Graf className={s.graf} /></p>
+                    <p className={s.calculations}>
+                        Перейти до розрахунків <Graf className={s.graf} />
+                    </p>
 
                     <p className={s.balance}>Баланс:</p>
                     <div className={s.balanceContainer}>
-                        <input type="number" placeholder="00.00 UAH" className={s.balanceInput} />
-                        <button type="button" className={s.balanceButton}>підтвердити</button>
+                        <input
+                            type="number"
+                            placeholder="00.00 UAH"
+                            className={s.balanceInput}
+                            value={balanceInput}
+                            onChange={(e) => setBalanceInput(e.target.value)}
+                        />
+                        <button
+                            type="button"
+                            className={s.balanceButton}
+                            onClick={handleConfirmBalance}
+                        >
+                            підтвердити
+                        </button>
                     </div>
                 </div>
             </Container>
         </section>
     );
-}
+};
