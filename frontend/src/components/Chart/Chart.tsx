bd80d681e13b7eb19ae3d57cbc4c901a616aba9e
DmitriySavin <debilv556@gmail.com>
2026-08-08 01:24:05 +0300

    add All Chart

--- frontend/src/components/Chart/Chart.tsx
+++ frontend/src/components/Chart/Chart.tsx
@@ -1,5 +1,5 @@
 import type { Transaction } from "@/redux/transaction_selectors";
-import { BiData } from "react-icons/bi";
+
 import {
   ResponsiveContainer,
   CartesianGrid,
@@ -10,24 +10,6 @@ import {
   Bar,
 } from "recharts";
 
-// interface Category {
-//   name: string;
-//   value: number;
-//   color: string;
-// }
-
-// const data: Category[] = [
-//   { name: "Свинина", value: 5000, color: "#FF751D" },
-//   { name: "Говядина", value: 4500, color: "#FFDAC0" },
-//   { name: "Курятина", value: 3200, color: "#FFDAC0" },
-//   { name: "Риба", value: 2100, color: "#FF751D" },
-//   { name: "Паніни", value: 1800, color: "#FFDAC0" },
-//   { name: "Кава", value: 1700, color: "#FFDAC0" },
-//   { name: "Спагетті", value: 1500, color: "#FF751D" },
-//   { name: "Шоколад", value: 800, color: "#FFDAC0" },
-//   { name: "Маслини", value: 500, color: "#FFDAC0" },
-//   { name: "Зелень", value: 300, color: "#FF751D" },
-// ];
 
 interface ChartData {
   bigData: Transaction[];
@@ -50,8 +32,6 @@ const customBar = (data: any) => {
 };
 
 const Chart = ({ bigData }: ChartData) => {
-  console.log(bigData);
-  // const newData = data.map(({category , ...}) )
 
   return bigData ? (
     <ResponsiveContainer width="50%" height={400} style={{ margin: "0 auto" }}>
@@ -68,15 +48,3 @@ const Chart = ({ bigData }: ChartData) => {
 
 export default Chart;
 
-// ResponsiveContainer - контейнер в котором храниться весь график , принимает value="100%"* && height="400"*
-// BarChart - контейнер для самого графика - принимает data[array[{}]](массив обьектов)
-// CartesianGrid - сетка фона(верт или гориз) vertical={false} будут горизонтальные
-// Xa - Ya принимает dataKey="name" или hide
-// ToolTip - подсказка при наведении вроде
-// Bar - столбец который принимает dataKey="value" , radius={[10(lt) , 10(rt) , 0(lb) , 0(lr)]}
-
-// клик по категории сохраняет\отправляет payload=[
-// {name="..." , value="123" , color="#11111" }
-// ]
-//
-//
